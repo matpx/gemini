57a7017a442ff7003b69e80ed2690a8f783f7c52
matpx <matpx@users.noreply.github.com>
2021-01-21 20:42:58 +0100

    rename position to translation

--- src/components.rs
+++ src/components.rs
@@ -1,6 +1,6 @@
 #[derive(Debug, Clone, Copy)]
 pub struct TransformComponent {
-    pub position: glam::Vec3,
+    pub translation: glam::Vec3,
     pub scale: glam::Vec3,
     pub rotation: glam::Quat,
     pub model: glam::Mat4,
@@ -9,7 +9,7 @@ pub struct TransformComponent {
 impl Default for TransformComponent {
     fn default() -> Self {
         Self {
-            position: glam::Vec3::zero(),
+            translation: glam::Vec3::zero(),
             scale: glam::Vec3::one(),
             rotation: glam::Quat::identity(),
             model: glam::Mat4::identity(),
--- src/systems/transform.rs
+++ src/systems/transform.rs
@@ -6,7 +6,7 @@ pub fn transform_system(world: &mut World) {
         transform.model = glam::Mat4::from_scale_rotation_translation(
             transform.scale,
             transform.rotation,
-            transform.position,
+            transform.translation,
         );
     }
 }
