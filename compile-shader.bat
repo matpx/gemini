d56e7e5ed7287ed0c144cd3cc26eafc3bc3c6e25
matpx <matpx@users.noreply.github.com>
2021-02-02 16:43:22 +0100

    remove glsl

--- compile-shader.bat
+++
@@ -1,2 +0,0 @@
-..\glslang\bin\glslangValidator.exe ./src/shader/unlit.frag -o ./src/shader/unlit.frag.spv -V
-..\glslang\bin\glslangValidator.exe ./src/shader/unlit.vert -o ./src/shader/unlit.vert.spv -V
